453c884a54b2812b24e9941740f591def6eef078
bernadette <bsd231@nyu.edu>
2025-03-12 12:31:39 -0400

    readded sendNativeMessage

--- background.js
+++ background.js
@@ -2,20 +2,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log('Message received: ', message);
   // 2. A page requested  data, respond with that data
   if (message.title) {
+    // gotta fix native host has exited
+    chrome.runtime.sendNativeMessage(
+      'com.automatedbooks.convert_and_combine_pdfs',
+      {ping: "ping"},
+      function (response) {
+        console.log('Received ' + response);
+      }
+    );
     sendResponse(`${message.title}\n${message.href}`);
     return true;
   }
   return true;
 });
 
-// // gotta fix native host has exited
-// chrome.runtime.sendNativeMessage(
-//   'com.automatedbooks.convert_and_combine_pdfs',
-//   {ping: "ping"},
-//   function (response) {
-//     console.log('Received ' + response);
-//   }
-// );
 
 chrome.action.onClicked.addListener(async (tab) => {
   await chrome.scripting.executeScript({
