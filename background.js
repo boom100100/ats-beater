c606ea22b7d89453c39dd88e0a608eb7da1b6377
bernadette <bsd231@nyu.edu>
2025-03-11 23:23:14 -0400

    implemented send port as mesage from background.js to content.js

--- background.js
+++ background.js
@@ -1,7 +1,69 @@
+// console.log(tab);
+// console.log("continuing setup");
+/*
+On startup, connect to the "com.automatedbooks.convert_and_combine_pdfs" app.
+*/
+// let port = browser.runtime.connectNative("com.automatedbooks.convert_and_combine_pdfs");
+// let port = chrome.runtime.connectNative("/Users/bernadette/Desktop/github/convert_and_combine_pdfs/convert_and_combine_pdfs.sh");
+// let port = chrome.runtime.connectNative("com.automatedbooks.convert_and_combine_pdfs");
+// let port = chrome.runtime.connectNative("com.automatedbooks.convert_and_combine_pdfs");
+// const port = {
+//   onMessage: { addListener: (arg) => arg},
+//   onDisconnect: { addListener: (arg) => arg},
+//   postMessage: (arg) => console.log(arg),
+// }
+
+// let port; 
+// // port = chrome.runtime.connectNative("com.automatedbooks.convert_and_combine_pdfs");
+// if (!port) {
+//   // this will run the first time to launch the native host, and save the connection to that instance in the port
+//   console.log(chrome);
+//   // console.log();
+//   // port = chrome.runtime.connect('com.automatedbooks.convert_and_combine_pdfs');
+//   port = chrome.runtime.connect('fccgofolnpapjidppgokhjpbfkmbangp');
+//   // port = chrome.runtime.connectNative('com.automatedbooks.convert_and_combine_pdfs');
+// }
+
+
+
+
+
+
+
+
+
+// /*
+// Listen for messages from the app and log them to the console.
+// */
+// port.onMessage.addListener((response) => {
+//   console.log("Received: " + response);
+// });
+
+// /*
+// Listen for the native messaging port closing.
+// */
+// port.onDisconnect.addListener((port) => {
+//   if (port.error) {
+//     console.log(`Disconnected due to an error: ${port.error.message}`);
+//   } else {
+//     // The port closed for an unspecified reason. If this occurred right after
+//     // calling `browser.runtime.connectNative()` there may have been a problem
+//     // starting the the native messaging client in the first place.
+//     // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#troubleshooting
+//     console.log(`Disconnected`, port);
+//   }
+// });
+
 chrome.action.onClicked.addListener(async (tab) => {
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id, allFrames: true },
-        files: ["scripts/content.js"],
-        
+  // `.query` waits for the content script to be available.
+  // `.send` sends the message.
+  chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {port: "sm"}, function(response) {
+        console.log(response);
     });
+  }); 
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true },
+    files: ["scripts/content.js"],
+  });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,67 +1,67 @@
-/*
-On startup, connect to the "com.automatedbooks.convert_and_combine_pdfs" app.
-*/
-let port = browser.runtime.connectNative("com.automatedbooks.convert_and_combine_pdfs");
+console.log("content ran");
 
-/*
-Listen for messages from the app and log them to the console.
-*/
-port.onMessage.addListener((response) => {
-  console.log("Received: " + response);
-});
+let port;
 
-/*
-Listen for the native messaging port closing.
-*/
-port.onDisconnect.addListener((port) => {
-  if (port.error) {
-    console.log(`Disconnected due to an error: ${port.error.message}`);
-  } else {
-    // The port closed for an unspecified reason. If this occurred right after
-    // calling `browser.runtime.connectNative()` there may have been a problem
-    // starting the the native messaging client in the first place.
-    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#troubleshooting
-    console.log(`Disconnected`, port);
-  }
-});
-
-/*
-When the extension's action icon is clicked, send the app a message.
-*/
-browser.browserAction.onClicked.addListener(() => {
-  // TODO: does it work: send the current page url, 
-    // hardcode required values
-  // TODO: mvp: send the (url, resume local file, open, and delete) request
-    // have the native extension receive inputs
-
-  // DEFAULT_COUNT = 1; // how many local files to choose
-  // DEFAULT_LOCAL_FILE_PATHS = ["/Users/bernadette/Downloads/Bernadette Davis Professional Resume Long.pdf"]; // resume file path // TODO: persist this value in the extension because the resume will likely be reused. CRUD
-  // DEFAULT_OUTPUT_FILE_NAME_WITHOUT_EXTENSION = "output"; // combined file name // TODO: make gui in chrome extension to set this value.
-  // DEFAULT_OUTPUT_DIRECTORY = "/Users/bernadette/Downloads"; // destination for generated files
-  // DEFAULT_WEBPAGE_URLS = [document.location.href]; // webpages to convert to pdf, download, and combine
-
-  // // // DEFAULT_WEBPAGE_URLS = [document.location.href];
-  // // // const DEFAULT_COUNT = 1; // how many local files to choose
-  // // // const SUPPLIED_OUTPUT_FILE_NAME_WITHOUT_EXTENSION = undefined;
-  // // // const SUPPLIED_LOCAL_FILE_PATHS = undefined || []; // TODO: persist this value in the extension because the resume will likely be reused. CRUD
-  const body = {
-    env_vars: {
-      WEBPAGE_URLS: [document.location.href],
-    },
-    // "-n": SUPPLIED_OUTPUT_FILE_NAME_WITHOUT_EXTENSION,
-    "-n": `${document.title} resume`,
-    "-o": true,
-    "-x": true,
-  }
+const handleMessages = (message, sender, sendResponse) => {
+  console.log("handleMessages ran");
+  port = message.port;
   
-  // set -c if select resume selected option is new resume
-  if ([].length === 0) {
-    body["-c"] = 1;
-  } else {
-    body.env_vars.LOCAL_FILE_PATHS = ["/Users/bernadette/Downloads/Bernadette Davis Professional Resume Long.pdf"];
+  console.log("message", message, "port", port);
+  return true;
+}
+
+chrome.runtime.onMessage.addListener(handleMessages);
+
+console.log(`port ${port}`);
+
+/*
+When submit is triggered, send the app a message.
+*/
+const submit = () => {
+  if (port === undefined) {
+    return;
   }
 
-  console.log(`Sending:  ${body}`);
-  port.postMessage(body);
-  // port.postMessage("ping");
-});
+  port.postMessage("ping");
+
+  
+  
+  // // console.log("submit");
+  //   // TODO: does it work: send the current page url, 
+  //     // hardcode required values
+  //   // TODO: mvp: send the (url, resume local file, open, and delete) request
+  //     // have the native extension receive inputs
+  
+  //   // DEFAULT_COUNT = 1; // how many local files to choose
+  //   // DEFAULT_LOCAL_FILE_PATHS = ["/Users/bernadette/Downloads/Bernadette Davis Professional Resume Long.pdf"]; // resume file path // TODO: persist this value in the extension because the resume will likely be reused. CRUD
+  //   // DEFAULT_OUTPUT_FILE_NAME_WITHOUT_EXTENSION = "output"; // combined file name // TODO: make gui in chrome extension to set this value.
+  //   // DEFAULT_OUTPUT_DIRECTORY = "/Users/bernadette/Downloads"; // destination for generated files
+  //   // DEFAULT_WEBPAGE_URLS = [document.location.href]; // webpages to convert to pdf, download, and combine
+  
+  //   // // // DEFAULT_WEBPAGE_URLS = [document.location.href];
+  //   // // // const DEFAULT_COUNT = 1; // how many local files to choose
+  //   // // // const SUPPLIED_OUTPUT_FILE_NAME_WITHOUT_EXTENSION = undefined;
+  //   // // // const SUPPLIED_LOCAL_FILE_PATHS = undefined || []; // TODO: persist this value in the extension because the resume will likely be reused. CRUD
+  //   const SUPPLIED_LOCAL_FILE_PATHS = []; // TODO: persist this value in the extension because the resume will likely be reused. CRUD
+  //   const body = {
+  //     env_vars: {
+  //       WEBPAGE_URLS: [document.location.href],
+  //     },
+  //     // "-n": SUPPLIED_OUTPUT_FILE_NAME_WITHOUT_EXTENSION,
+  //     "-n": `${document.title} resume`,
+  //     "-o": true,
+  //     "-x": true,
+  //   }
+  
+  //   // set -c if select resume selected option is choose new resume
+  //   if (SUPPLIED_LOCAL_FILE_PATHS.length === 0) {
+  //     body["-c"] = 1;
+  //   } else {
+  //     body.env_vars.LOCAL_FILE_PATHS = ["/Users/bernadette/Downloads/Bernadette Davis Professional Resume Long.pdf"];
+  //   }
+  
+  //   console.log(`Sending:  ${body}`);
+  //   port.postMessage(body);
+};
+
+submit();
